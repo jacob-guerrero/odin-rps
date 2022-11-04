23e4fb3d0b319ffb6068b035076678b69fc594e0
Jacob <j31c0@hotmail.com>
2022-11-04 11:37:50 -0500

    Add random function and conditional

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("test")
+function getComputerChoice() {
+    return Math.floor(Math.random() * 3);
+    
+}
+let computerChoice;
+if (getComputerChoice() == 0) {
+    computerChoice = "Rock";
+} else if (getComputerChoice() == 1) {
+    computerChoice = "Paper";
+} else {
+    computerChoice = "Scissors";
+}
+console.log (computerChoice)

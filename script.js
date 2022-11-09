9c79b01b60aa60ec13de2cd70f16a08cf3ab0e27
Jacob <j31c0@hotmail.com>
2022-11-09 00:19:47 -0500

    Automate game process

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice == 0) {
-        return "Rock";
+        return "rock";
     } else if (computerChoice == 1) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
-        return "It's a draw!";
+        console.log("It's a draw!");
     } else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")){
-        return `You Win!, ${playerSelection} beats ${computerSelection}`;
+        console.log(`You Win!, ${playerSelection} beats ${computerSelection}`);
     } else {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
     }
 }
-let playerSelection = "paPEr".toLowerCase();
-let computerSelection = getComputerChoice().toLowerCase();
 
+function updateComputerChoice() {
+    computerSelection = getComputerChoice();
+}
 
-console.log(playRound(playerSelection, computerSelection));
+function updatePlayerSelection() {
+    playerSelection = prompt("Rock, Paper, Scissors").toLowerCase();
+}
 
-/* let playerSelection = "Rock".toLowerCase();
-let computerSelection = getComputerChoice().toLowerCase();
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        return "It's a draw!";
-    } else {
-        return "no";
+function game() {
+    for (let i = 0; i < 5; i++) {
+        updatePlayerSelection();
+        updateComputerChoice();
+        playRound(playerSelection, computerSelection);
     }
-} */
-/* 
+}
 
-console.log (getComputerChoice());
-console.log (playerSelection);
-console.log (playRound()); */
+game();

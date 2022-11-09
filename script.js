ae0d145f3b89f80cc18e2ae8a29b897ecda10d06
Jacob <j31c0@hotmail.com>
2022-11-08 23:01:21 -0500

    Add conditionals

--- script.js
+++ script.js
@@ -9,4 +9,27 @@ function getComputerChoice() {
     }
 }
 
-console.log (getComputerChoice()) 
+let playerSelection = "paPEr".toLowerCase();
+let computerSelection = getComputerChoice().toLowerCase();
+if (playerSelection == computerSelection) {
+    console.log("It's a draw!");
+} else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")){
+    console.log(`You Win!, ${playerSelection} beats ${computerSelection}`);
+} else {
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
+}
+
+/* let playerSelection = "Rock".toLowerCase();
+let computerSelection = getComputerChoice().toLowerCase();
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "It's a draw!";
+    } else {
+        return "no";
+    }
+} */
+/* 
+
+console.log (getComputerChoice());
+console.log (playerSelection);
+console.log (playRound()); */
